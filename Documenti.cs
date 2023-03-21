7a4badf53677bb2039cd1ea713d72bdf9b7a3d5a
Gabriele Pagnotta <gabriele.pagnotta1998@gmail.com>
2023-03-21 18:16:02 +0100

    ereditarietà(forse)

---
+++ Documenti.cs
@@ -0,0 +1,46 @@
+﻿
+
+public class Documenti
+{
+    public int Codice { get; set; }
+    public string Titolo { get; set; }
+    public int Anno { get; set; }
+    public string Settore { get; set; }
+    public int Scaffale { get; set; }
+    public string Autore { get; set; }
+
+    public Documenti(int codice, string titolo, int anno, string settore, int scaffale, string autore)
+    {
+        Codice = codice;
+        Titolo = titolo;
+        Anno = anno;
+        Settore = settore;
+        Scaffale = scaffale;
+        Autore = autore;
+
+    }
+
+   
+}
+
+
+public class Libro:Documenti
+{
+    public Libro(int codice, string titolo, int anno, string settore, int scaffale, string autore) : base(codice, titolo, anno, settore, scaffale, autore)
+    {
+    }
+
+    public int Numeropagine { get; set; }
+    
+
+}
+
+public class Dvd : Documenti
+{
+    public double durata;
+
+    public Dvd(int codice, string titolo, int anno, string settore, int scaffale, string autore) : base(codice, titolo, anno, settore, scaffale, autore)
+    {
+    }
+}
+
---
+++ Utente.cs
@@ -0,0 +1,17 @@
+﻿public class Utente
+{
+    public string Nome;
+    public string Cognome;
+    public string Email;
+    public string Telefono;
+    public string Password;
+
+    public Utente(string nome, string cognome, string email, string telefono,string password)
+    {
+        Nome = nome;
+        Cognome = cognome;
+        Email = email;
+        Password = password;
+        Telefono = telefono;
+    }
+}
